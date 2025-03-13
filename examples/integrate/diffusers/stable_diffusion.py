4fae3b07287197e8c0c166b097b5a71aa3ba30b1
Taojie Wang <taojie.sustcs@gmail.com>
2025-03-13 17:06:28 +0800

    Init similar cache for image generation

--- examples/integrate/diffusers/stable_diffusion.py
+++ examples/integrate/diffusers/stable_diffusion.py
@@ -3,14 +3,11 @@ import sys
 gptcache_path = '/home/taojie_wang/GPTCache'
 sys.path.insert(0, gptcache_path)
 
-import gptcache
-print(gptcache.__file__)
-
-
 data_dir = '/home/taojie_wang/GPTCache/multimodal'
 import shutil
 import os
 if os.path.isdir(data_dir):
+    print(f'removing the dir {data_dir}.')
     shutil.rmtree(data_dir)
 
 
@@ -26,23 +23,6 @@ from gptcache.embedding import (
 from gptcache.manager import get_data_manager, CacheBase, VectorBase, ObjectBase
 from gptcache.manager import manager_factory
 from gptcache.similarity_evaluation.distance import SearchDistanceEvaluation
-
-# onnx = Onnx()
-# cache_base = CacheBase('sqlite')
-# vector_base = VectorBase('faiss', dimension=onnx.dimension, index_path='./local/faiss20.index'),
-# print(f"type of vector base: {type(vector_base)}")
-# vector_base = vector_base[0]
-# object_base = ObjectBase('local', path='./images')
-# data_manager = get_data_manager(cache_base, vector_base, object_base)
-
-# # this is the gloval cache variable
-# cache.init(
-#     pre_embedding_func=get_prompt,
-#     embedding_func=onnx.to_embeddings,
-#     data_manager=data_manager,
-#     similarity_evaluation=SearchDistanceEvaluation(),
-#     )
-
 
 the_cache = Cache()
 embedding=Huggingface()
@@ -70,13 +50,14 @@ pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
 # prompt = "a photo of a cat. DO NOT GENERATE THIS: a photo of an astronaut riding a horse on mars"
-prompt = "a photo of a cat.  a photo of an astronaut riding a horse on mars"
+prompt1 = "a photo of an astronaut riding a horse on mars"
+prompt2 = "An image depicting an astronaut mounted on a horse on the planet Mars"
 start = time.time()
-image1 = pipe(prompt=prompt, cache_obj=the_cache).images[0]
+image1 = pipe(prompt=prompt1, cache_obj=the_cache).images[0]
 print("First time generation:", time.time() - start)
 
 start = time.time()
-image2 = pipe(prompt=prompt, cache_obj=the_cache).images[0]
+image2 = pipe(prompt=prompt2, cache_obj=the_cache).images[0]
 print("Second time generation:", time.time() - start)
 
 # Compare generated images
@@ -84,7 +65,7 @@ diff = ImageChops.difference(image1, image2)
 assert not diff.getbbox(), "Got different images."
 
 # # save images
-# image1.save("/home/taojie_wang/GPTCache/multimodal/images/astronaut_horse_mars_1.png")
-# image2.save("/home/taojie_wang/GPTCache/multimodal/images/astronaut_horse_mars_2.png")
+image1.save(f"/home/taojie_wang/GPTCache/images/{time.time()}.png")
+image2.save(f"/home/taojie_wang/GPTCache/images/{time.time()}.png")
 
 print("we are done")
