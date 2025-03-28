7332ed63ccaba530effa6f3a2f9dd64574be2109
Taojie Wang <1138535467@qq.com>
2025-03-28 11:18:54 +0000

    prepare formatted question and wrong answers

--- poisoning/evaluations/crafting_prompts/black.py
+++
@@ -1,9 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline("text-generation", model="meta-llama/Llama-3.1-8B-Instruct")
-completion = pipe(messages)
-print(completion)
