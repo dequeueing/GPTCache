2dc2e794281381cc189dcfcb473fd9c3bbba7e60
Taojie Wang <1138535467@qq.com>
2025-04-05 16:42:11 +0000

    get noise sampled

--- poisoning/evaluations/data_prepare/text-to-image/datasets_raw/get_all_noise.py
+++ poisoning/evaluations/data_prepare/text-to-image/datasets_raw/get_all_noise.py
@@ -1,9 +1,7 @@
 import json
+import random
 
-
-input_path = "/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/data_prepare/text-to-image/datasets_raw/flickr30k.json"
-output_path = "/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/data_prepare/text-to-image/datasets_raw/all_noise.json"
-if __name__ == '__main__':
+def get_all_noise(input_path, output_path):
     with open(input_path) as file:
         data = json.load(file)
         
@@ -11,3 +9,20 @@ if __name__ == '__main__':
     
     with open(output_path, 'w') as f:
         json.dump(all_noise, f, indent=2)
+        
+def sample_noise(input_path, output_path, noise_number):
+    with open(input_path) as file:
+        data = json.load(file)
+        
+    data = random.sample(data, noise_number)
+    
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+
+output_path = "/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/data_prepare/text-to-image/datasets_raw/noise.json"
+input_path = "/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/data_prepare/text-to-image/datasets_raw/all_noise.json"
+
+if __name__ == '__main__':
+    sample_noise(input_path, output_path, noise_number=500)
