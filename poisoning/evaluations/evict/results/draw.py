44ec2b8c7d118fdf090e5b56b6492181d2e555a3
Taojie Wang <1138535467@qq.com>
2025-04-07 15:06:13 +0000

    evict and maintain finished

--- poisoning/evaluations/evict/results/draw.py
+++ poisoning/evaluations/evict/results/draw.py
@@ -3,55 +3,37 @@ import matplotlib.pyplot as plt
 
 
 dataset_id = "squad"
-cache_sizes = [
-    500
-    # 500, 1000, 2000
-]
-user_frequencys = {
-    1: {'t':[], 'freq': [], 'number': []}, 
-    2: {'t':[], 'freq': [], 'number': []}, 
-    4: {'t':[], 'freq': [], 'number': []}
-}
+cache_sizes = [500, 1000, 2000]
+user_numbers = [1, 2, 3, 4, 5, 6]
+
 
 if __name__ == "__main__":
+    plt.figure()
     for cache_size in cache_sizes:
         input_file = f"{dataset_id}_{cache_size}.json"
         with open(input_file, 'r') as file:
             data = json.load(file)
             
-        t_axis = []
-        attacker_number = []
-        attacker_frequency = []
-        for i in range(len(data)):
-            end_user_send_number = i + 1
+        attacker_freqs = []
+        victim_total_query_number = len(data)
+        for user_num in user_numbers:
+            sending_freq = user_num * 1
+            time_needed = victim_total_query_number / sending_freq
+            attacker_freqs.append(1 / time_needed)
+                
             
-            # the independent var is user frequency
-            for user_frequency in user_frequencys: 
-                t = end_user_send_number / user_frequency
-                number_attacker_to_send = data[i]
-                frequency_attacker_to_send = data[i] / t
-                
-                t_axis = user_frequencys[user_frequency]['t']
-                attacker_number = user_frequencys[user_frequency]['number']
-                attacker_frequency = user_frequencys[user_frequency]['freq']
-                
-                t_axis.append(t)
-                attacker_number.append(number_attacker_to_send)
-                attacker_frequency.append(frequency_attacker_to_send)
-        
-        # Plot 1: Attacker Number vs. Time
-        plt.figure()
-        for user_frequency in user_frequencys:
-            plt.plot(
-                user_frequencys[user_frequency]['t'],
-                user_frequencys[user_frequency]['number'],
-                label=f"user_freq={user_frequency}"
-            )
-        plt.xlabel("Time (second)")
-        plt.ylabel("Attacker Number")
-        plt.title(f"Attacker Number vs. Time (Cache Size: {cache_size})")
-        plt.grid(True)
-        plt.legend()
-        plt.savefig(f"attacker_number_{cache_size}.png")
-        plt.close()
+        plt.plot(
+            [item * 250 for item in user_numbers],
+            attacker_freqs,
+            label=f"cache size={cache_size}"
+        )
+
+                    
+    plt.xlabel("User number")
+    plt.ylabel("Attacker Maintain Frequency (1 / second)")
+    plt.title(f"Attacker Maintain Frequency vs. User number")
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(f"Maintain.png")
+    plt.close()
 
