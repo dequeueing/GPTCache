87baa8cc7799373b49a75dc47ec09b5d3a92a631
Taojie Wang <1138535467@qq.com>
2025-04-07 13:06:08 +0000

    evict get results

--- poisoning/evaluations/evict/run.py
+++ poisoning/evaluations/evict/run.py
@@ -69,13 +69,19 @@ def get_config(config:str, value):
 # 250 users -> 1 query per second
 configs = {
     # 'user number': [250, 500, 1000, 2000],
-    'user number': [250],
-    # 'cache size': [500, 1000, 1500, 2000]
+    # 'user number': [250],
+    'cache size': [500, 1000, 2000]
 }
 default = {
     'user number': 250,
     'cache size': 500
 }
+
+datasets =  [
+    "squad",
+    "MedQuad-MedicalQnADataset",
+    "ms_marco"
+]
 
 
 
@@ -84,79 +90,87 @@ if __name__ == '__main__':
     random.seed(42)
     
     # init cache
-    for config in configs:
-        for value in configs[config]:
-            user_number, cache_size = get_config(config, value)
-            
-    setup_cache(cache_size)
-    
-    # load noise
-    noise_file = '/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/evict/data/squad.json'
-    with open(noise_file, 'r') as f:
-        noise = json.load(f)
-        
-    noise_question = [item['question'] for item in noise]
-    noise_id = [item['id'] for item in noise]
-    
-    # Change: if not hit, will return the same prompt as itself.
-        
-    # inject noise 
-    start = time.time()
-    selected_noise_id = set()
-    injected_noise_id = set()
-    while len(injected_noise_id) < cache_size:
-        random_question = random.choice(noise)
-        random_question_str = random_question['question']
-        random_question_id = random_question['id']
-        
-        # check 
-        if random_question_id in selected_noise_id:
-            continue
-        selected_noise_id.add(random_question_id)
-        
-        # try inject
-        completion = generate(random_question_str)
-        if completion == random_question_str:
-            injected_noise_id.add(random_question_id)
-            
-        print(f"current cache number: {get_cache_size()}")
-            
-    print(f"injection finished")
-    print(f"time taken: {time.time() - start}")
-    
-    # simulate end users; default: 1 query per second
-    # so the user frequency can be summarized as how many queries the user send; and the time varies based on frequency
-    # 5000 end user queries
-    chosen_end_user_id = set()
-    injected_end_user_id = set()  # len of this set is the newly injected into
-    attacker_to_inject = []
-    for i in range(0, 5000):
-        # select user query, excluded from the chosen ones
-        while True:
-            random_question = random.choice(noise)
-            random_question_str = random_question['question']
-            random_question_id = random_question['id']
-            if random_question_id not in chosen_end_user_id and random_question_id not in selected_noise_id:
-                chosen_end_user_id.add(random_question_id)
-                break
-            
-        # inject
-        response = generate(random_question_str)
-        if response == random_question_str:  # inject success
-            injected_end_user_id.add(random_question_id)
-        
-        # record attacker data 
-        attacker_to_inject.append(cache_size - len(injected_end_user_id))
-        
-        # finish eviction
-        if len(injected_end_user_id) == cache_size:
-            break
-        
-    # draw
-    plt.plot(attacker_to_inject)
-    plt.title("Eviction Evaluation")
-    plt.xlabel("Number of user injection")
-    plt.ylabel("Number of attacker injection")
-    plt.grid(True)
-    plt.show()
-    plt.savefig("attacker_to_inject_plot.png")
+    for dataset_id in datasets:
+        for config in configs:
+            for value in configs[config]:
+                user_number, cache_size = get_config(config, value)
+                
+                setup_cache(cache_size)
+                
+                # load noise
+                noise_file = f'/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/evict/data/{dataset_id}.json'
+                output_file = f'/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/evict/results/{dataset_id}_{value}.json'
+                png_file = f'/home/taojie_wang@idm.teecertlabs.com/GPTCache/poisoning/evaluations/evict/pngs/{dataset_id}_{value}.png'
+                with open(noise_file, 'r') as f:
+                    noise = json.load(f)
+                    
+                noise_question = [item['question'] for item in noise]
+                noise_id = [item['id'] for item in noise]
+                
+                # Change: if not hit, will return the same prompt as itself.
+                    
+                # inject noise 
+                start = time.time()
+                selected_noise_id = set()
+                injected_noise_id = set()
+                while len(injected_noise_id) < cache_size:
+                    random_question = random.choice(noise)
+                    random_question_str = random_question['question']
+                    random_question_id = random_question['id']
+                    
+                    # check 
+                    if random_question_id in selected_noise_id:
+                        continue
+                    selected_noise_id.add(random_question_id)
+                    
+                    # try inject
+                    completion = generate(random_question_str)
+                    if completion == random_question_str:
+                        injected_noise_id.add(random_question_id)
+                        
+                    print(f"current cache number: {get_cache_size()}")
+                        
+                print(f"injection finished")
+                print(f"time taken: {time.time() - start}")
+                
+                # simulate end users; default: 1 query per second
+                # so the user frequency can be summarized as how many queries the user send; and the time varies based on frequency
+                # 5000 end user queries
+                chosen_end_user_id = set()
+                injected_end_user_id = set()  # len of this set is the newly injected into
+                attacker_to_inject = []
+                for i in range(0, 7000):
+                    print(f"current {i}")
+                    # select user query, excluded from the chosen ones
+                    while True:
+                        random_question = random.choice(noise)
+                        random_question_str = random_question['question']
+                        random_question_id = random_question['id']
+                        if random_question_id not in chosen_end_user_id and random_question_id not in selected_noise_id:
+                            chosen_end_user_id.add(random_question_id)
+                            break
+                        
+                    # inject
+                    response = generate(random_question_str)
+                    if response == random_question_str:  # inject success
+                        injected_end_user_id.add(random_question_id)
+                    
+                    # record attacker data 
+                    attacker_to_inject.append(cache_size - len(injected_end_user_id))
+                    
+                    # finish eviction
+                    if len(injected_end_user_id) == cache_size:
+                        break
+                    
+                    
+                with open(output_file, 'w') as file:
+                    json.dump(attacker_to_inject, file, indent=4)
+
+                    
+                # draw
+                plt.plot(attacker_to_inject)
+                plt.title(f"Eviction Evaluation {dataset_id}_{value}")
+                plt.xlabel("Number of user injection")
+                plt.ylabel("Number of attacker injection")
+                plt.grid(True)
+                plt.savefig(png_file)
