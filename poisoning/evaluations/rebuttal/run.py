41e1f75d5c36d7d60270660306c4b5bd79a8fdc0
Taojie Wang <1138535467@qq.com>
2025-06-09 20:00:50 +0000

    prepare for rebuttal

---
+++ poisoning/evaluations/rebuttal/run.py
@@ -0,0 +1,235 @@
+import json
+import random
+import shutil
+import os
+import numpy as np
+from util import *
+from typing import *
+
+def generate(prompt):
+    prompt = after_template(prompt, tokenizer)
+    completion = cached_llm.invoke(prompt, cache_obj=the_cache)
+    completion = completion.rstrip('.')
+    return completion
+
+def get_config(config:str, value):
+    (threshold, top_k, noise_number, correlation) = (default['thresholds'], default['top_k'], default['noise_number'], default['correlation'])
+    if config == 'thresholds':
+        threshold = value
+    if config == 'top_k':
+        top_k = value
+    if config == 'noise_number':
+        noise_number = value
+    if config == 'correlation':
+        correlation = value
+    return threshold, top_k, noise_number, correlation
+
+def setup_cache(threshold, top_k, noise_number):
+    global cached_llm
+    llm = HuggingFacePipeline(pipeline=pipe)
+    cached_llm = LangChainLLMs(llm=llm)
+
+    # Clean up the index
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+    shutil.rmtree(data_dir)
+
+    # Init cache
+    embedding=Huggingface()
+    data_manager = manager_factory(
+                "sqlite,faiss",
+                data_dir=data_dir,
+                vector_params={"dimension": embedding.dimension, "top_k": top_k},
+                eviction_params={'max_size': max(noise_number, 1000)}, 
+            )
+    init_similar_cache(
+        data_dir=data_dir,
+        cache_obj=the_cache,
+        pre_func=extract_user_question,
+        embedding=embedding,
+        data_manager=data_manager,
+        evaluation=SbertCrossencoderEvaluation(),
+        post_func=from_list,
+        config=Config(similarity_threshold=threshold),
+    )
+    
+def inject_noise(target_question:str, correlation, noise_number,  all_noise):
+    # Extract noise questions and IDs
+    # noise_questions = [noise['question'] for noise in all_noise]
+
+    # # Compute cosine similarity in batches
+    # batch_size = 2048
+    # all_similarities = []
+
+    # for i in range(0, len(noise_questions), batch_size):
+    #     batch_noise_questions = noise_questions[i:i + batch_size]
+    #     batch_similarities = cosine_sim_batch(batch_noise_questions, [target_question] * len(batch_noise_questions))
+        
+    #     # Store computed similarities
+    #     all_similarities.extend(batch_similarities)
+
+    # # Attach cosine similarity to each noise sample
+    # for i, noise in enumerate(all_noise):
+    #     noise["dist"] = all_similarities[i]
+    # selected_ids = get_normal_distribution(dist_mean=correlation, number=noise_number, all_noise=all_noise)
+    # selected_noise = [noise_questions[id] for id in selected_ids]
+    
+    for target_question in all_noise:
+        put(target_question, f"This is a noise question: {target_question}", cache_obj=the_cache)
+        # generate(target_question)
+        
+    # Check the questions are in the cache
+    # vector_db = the_cache.data_manager.v  # Access the FAISS vector store
+    # vector_count = vector_db.count()
+    # assert vector_count == len(all_noise)
+
+def get_normal_distribution(dist_mean: float, number: int, all_noise, seed: int = 20):
+    """Return a subset of noise IDs where cosine similarity follows a normal distribution."""
+    if seed is not None:
+        np.random.seed(seed)  # Set the seed for reproducibility
+    cosine_similarities = np.array([noise['dist'] for noise in all_noise])
+    std_dev = np.std(cosine_similarities) if np.std(cosine_similarities) > 0 else 1  # Avoid division by zero
+    probabilities = np.exp(-0.5 * ((cosine_similarities - dist_mean) / std_dev) ** 2)
+    probabilities /= probabilities.sum()
+    selected_indices = np.random.choice(len(all_noise), size=number, p=probabilities, replace=False)
+    return [all_noise[i]['id'] for i in selected_indices]
+
+prompt_path = '/home/exouser/GPTCache/poisoning/evaluations/7.3/gptcache/prompts_new_dataset_white/'
+output_path = '/home/exouser/GPTCache/poisoning/evaluations/7.3/gptcache/results_new_dataset_white/'
+noise_path = '/home/exouser/GPTCache/poisoning/evaluations/7.3/gptcache/noise_new_dataset/'
+datasets = [
+    # 'hotpotqa',
+    # 'nq',
+    # 'trivia',
+    # 'wiki',
+    'ms_marco'
+]
+
+prompt_injection_patterns = [
+    'white',
+    # 'black'
+]
+
+configs = {
+    'thresholds': [0.8],  
+}
+
+default = {
+    'thresholds': 0.8,
+    'top_k': 5,
+    'noise_number': 500,
+    'correlation': 0.7
+}
+
+
+if __name__ == '__main__':
+    stat_file = output_path + f"E73_new_dataset_gptcache_summary.json"
+    try:
+        with open(stat_file, 'w') as file:
+            stat = json.load(file)
+    except Exception:
+        stat = []
+
+    for pattern in prompt_injection_patterns:
+        for dataset_id in datasets:
+            for config in configs:
+                for value in configs[config]:
+                    independent_var = value
+                    threshold, top_k, noise_number, correlation = get_config(config, independent_var)
+                    
+                    prompt_file = prompt_path + f"{dataset_id}.json"
+                    noise_file = noise_path + f"{dataset_id}.json"
+                    output_file = "/home/exouser/GPTCache/poisoning/evaluations/7.3/gptcache/results_new/E73_gptcache_white_ms_marco.json"
+
+                    # load noise and target
+                    with open(noise_file, 'r') as file:
+                        all_noise = json.load(file)
+                    with open(output_file, 'r') as f:  
+                        data = json.load(f)
+                    try:
+                        all_noise = [item['question'] for item in all_noise]
+                    except Exception:
+                        pass
+                        
+                    # sample noise, exclude the target question
+                    target_question = [item['question'] for item in data]
+                    all_noise = [item for item in all_noise if item not in target_question]
+                    all_noise = random.sample(all_noise, noise_number)
+                    
+                    attack_cnt = 0
+                    injection_cnt = 0
+                    similar_cnt = 0
+                    total = len(data)
+
+                    # Change: since each target question will have a noise set, 
+                    #   we have to inject the noise every time.
+                    for item in data:                
+                        # if item['attack success']:
+                        #     continue
+                        
+                        # get the sem and cosine data:
+                        # "cos_sim_white": 0.8056195974349976,
+                        # "sem_score_white": 0.9837311506271362,
+
+                                
+                        injection_success = False
+                        cached_success = True
+                        attack_success = False
+                        similar_enough = False
+                        
+                        question = item['question']
+                        adv = item['white'] if pattern == 'white' else item['black']
+                        target_answer = item['wrong']
+                        
+                        
+                        setup_cache(threshold, top_k, noise_number)
+                        inject_noise(question, correlation, noise_number,  all_noise)
+                        
+                        attacker_response = generate(adv)
+                        victim_response = generate(question)
+                        
+                        if 'This is a noise question' in attacker_response:
+                            cached_success = False
+                            
+                        target_answer = target_answer.rstrip('.')
+                        attacker_response = attacker_response.rstrip('.')
+                        victim_response = victim_response.rstrip('.')
+                        if attacker_response == target_answer:
+                            injection_success = True
+                    
+                                            
+                        if victim_response == attacker_response:
+                            similar_enough = True
+                        
+                        if similar_enough and injection_success and cached_success:
+                            attack_success = True
+                            
+                        if attack_success:
+                            attack_cnt += 1
+                        if similar_enough:
+                            similar_cnt += 1
+                                                                            
+                        item[f'attacker response WHITE'] = attacker_response
+                        item[f'victim response WHITE'] = victim_response
+                        item[f'attack success WHITE'] = attack_success 
+                        item[f'similar enough WHITE'] = similar_enough
+                        item[f'injection success WHITE'] = injection_success
+                        item['recheck WHITE'] = True
+                        
+                            
+                        # store adv to local
+                        with open(output_file, "w") as file:
+                            json.dump(data, file, indent=4)
+                            
+                            
+                    stat.append(
+                        {
+                        'Dataset': f"E73_gptcache_new_dataset_{pattern}{dataset_id}", 
+                        'ASR': attack_cnt / total,
+                        }
+                    )
+                    
+                    print(stat)
+                            
+                    with open(stat_file, 'w') as file:
+                        json.dump(stat, file, indent=4)
---
+++ poisoning/evaluations/rebuttal/util.py
@@ -0,0 +1,208 @@
+from langchain_huggingface import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel, BitsAndBytesConfig
+from transformers import pipeline
+import torch
+from typing import *
+import sys
+sys.path.insert(0, '/home/exouser/GPTCache')
+from gptcache.adapter.langchain_models import LangChainLLMs
+from gptcache.adapter.api import init_similar_cache, put
+from gptcache.core import Cache, Config
+from gptcache.processor.post import nop
+from gptcache.manager import manager_factory
+from gptcache.manager.vector_data.faiss import Faiss
+from gptcache.similarity_evaluation import *
+from gptcache.embedding import (
+    Huggingface,
+)
+
+
+# Load model and tokenizer
+embedding_model_type = 'distilbert-base-uncased'
+embedding_model = AutoModel.from_pretrained(embedding_model_type).to('cuda').eval()
+embedding_tokenizer = AutoTokenizer.from_pretrained(embedding_model_type)
+
+# Function for sentence embedding
+def _sent_embed_from_hidden(hidden_states, attention_mask):
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(hidden_states.size()).float()
+    ).to(hidden_states.device)
+    sentence_embs = torch.sum(hidden_states * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
+    return sentence_embs
+    
+# Optimized cosine similarity function
+def cosine_sim_batch(queries1, queries2):
+    queries1_tokenized = embedding_tokenizer(queries1, return_tensors="pt", padding=True, truncation=True).to('cuda')
+    queries2_tokenized = embedding_tokenizer(queries2, return_tensors="pt", padding=True, truncation=True).to('cuda')
+    
+    with torch.no_grad():
+        q1_embedding = embedding_model(**queries1_tokenized).last_hidden_state
+        q2_embedding = embedding_model(**queries2_tokenized).last_hidden_state
+
+        q1_sent_emb = _sent_embed_from_hidden(q1_embedding, queries1_tokenized['attention_mask'])
+        q2_sent_emb = _sent_embed_from_hidden(q2_embedding, queries2_tokenized['attention_mask'])
+        
+        return torch.nn.functional.cosine_similarity(q1_sent_emb, q2_sent_emb).cpu().numpy()
+
+
+
+# Load the tokenizer and model
+device = "cuda" 
+model_path = "meta-llama/Llama-3.1-8B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    trust_remote_code=True,
+    low_cpu_mem_usage=True,
+).to(device)
+
+# Create a Hugging Face pipeline for text generation
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=10,
+    do_sample=False,
+    return_full_text=False,  
+)
+
+
+def extract_user_question(data: Dict[str, Any], **_: Dict[str, Any]) -> Any:
+    input_string = data.get("prompt")
+    
+    # Define the markers
+    start_marker = "<|end_header_id|>"
+    end_marker = "<|eot_id|>"
+    
+    # Find the positions of the markers
+    start_pos = input_string.find(start_marker, input_string.find("user")) + len(start_marker)
+    end_pos = input_string.find(end_marker, start_pos)
+    
+    # Extract the substring between the markers
+    if start_pos != -1 and end_pos != -1:
+        question = input_string[start_pos:end_pos].strip()
+        return question
+    else:
+        return input_string
+    
+def from_list(messages: List[Any]) -> Any:
+    """No change after evaluation.
+
+    :param messages: A list of candidate outputs.
+    :type messages: List[Any]
+
+    Example:
+        .. code-block:: python
+
+            from gptcache.processor.post import nop
+
+            messages = ["message 1", "message 2", "message 3"]
+            answer = nop(messages)
+            assert answer = messages
+    """
+    return messages[0]
+
+
+def after_template(message, tokenizer):
+    prompt = [
+        {"role": "user", "content": message},
+    ]
+    return tokenizer.apply_chat_template(
+        prompt, tokenize=False, add_generation_prompt=True
+    )
+
+
+
+the_cache = Cache()
+data_dir = '.attack'
+cached_llm = None
+
+
+
+
+import torch
+from sentence_transformers import CrossEncoder
+from transformers import (
+    AutoModel,
+    AutoTokenizer,
+)
+
+embedding_model_type = 'distilbert-base-uncased'
+embedding_model = AutoModel.from_pretrained(embedding_model_type).to('cuda')
+embedding_tokenizer = AutoTokenizer.from_pretrained(embedding_model_type)
+
+def _sent_embed_from_hidden(hidden_states, attention_mask):
+    """convert token embedding to sentence embedding, mean the dimensions across all tokens"""
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(hidden_states.size()).float()
+    )
+    input_mask_expanded = input_mask_expanded.to(hidden_states.device)
+    sentence_embs = torch.sum(hidden_states * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
+    return sentence_embs
+
+def cosine_sim(query1, query2):
+    # Tokenization
+    query1_tokenized = embedding_tokenizer(query1, return_tensors="pt", padding=False).to(embedding_model.device)
+    query2_tokenized = embedding_tokenizer(query2, return_tensors="pt", padding=False).to(embedding_model.device)
+    
+    # Forward
+    q1_embedding = embedding_model(**query1_tokenized).last_hidden_state
+    q2_embedding = embedding_model(**query2_tokenized).last_hidden_state
+    
+    # Get sentence embedding from layer activation
+    q1_sent_emb = _sent_embed_from_hidden(q1_embedding, query1_tokenized['attention_mask']).squeeze(0)
+    q2_sent_emb = _sent_embed_from_hidden(q2_embedding, query2_tokenized['attention_mask']).squeeze(0)
+    
+    # Cosine similarity
+    return torch.nn.CosineSimilarity(dim=0)(
+        q1_sent_emb, q2_sent_emb
+    )
+
+
+def euclidean_distance(query1, query2):
+    # Tokenization
+    query1_tokenized = embedding_tokenizer(query1, return_tensors="pt", padding=False).to(embedding_model.device)
+    query2_tokenized = embedding_tokenizer(query2, return_tensors="pt", padding=False).to(embedding_model.device)
+    
+    # Forward
+    q1_embedding = embedding_model(**query1_tokenized).last_hidden_state
+    q2_embedding = embedding_model(**query2_tokenized).last_hidden_state
+    
+    # Get sentence embedding from layer activation
+    q1_sent_emb = _sent_embed_from_hidden(q1_embedding, query1_tokenized['attention_mask']).squeeze(0)
+    q2_sent_emb = _sent_embed_from_hidden(q2_embedding, query2_tokenized['attention_mask']).squeeze(0)
+    
+    # distance = torch.dist(q1_sent_emb, q2_sent_emb, p=2)
+    q1_sent_emb_norm = q1_sent_emb / torch.norm(q1_sent_emb, p=2)
+    q2_sent_emb_norm = q2_sent_emb / torch.norm(q2_sent_emb, p=2)
+    distance = torch.norm(q1_sent_emb_norm - q2_sent_emb_norm, p=2)
+    return distance
+
+model_id = "cross-encoder/quora-distilroberta-base"
+encoder = CrossEncoder(model_id)
+
+def semantic_score(q1, q2):
+    score = encoder.predict([(q1, q2)])[0]
+    return score
+
+def after_template(message, tokenizer):
+    prompt = [
+        {"role": "user", "content": message},
+    ]
+    return tokenizer.apply_chat_template(
+        prompt, tokenize=False, add_generation_prompt=True
+    )
+
+
+
+if __name__ == '__main__':
+    p1 = 'what is your name?'
+    p2 = 'Tell me your name?'
+    print(semantic_score(p1, p2))
+    print(euclidean_distance(p1, p2))
+    print(cosine_sim(p1, p2))
+    
